1339efb9de24005d2ac470658104e977a1eab3a2
Nik <nik@example.com>
2026-02-02 15:31:41 +0300

    3 commit - создал пройсто handler для бота, проверить работоспособность

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from src.presentation.api_bot.bot import bot_main
 import asyncio
-
+import logging
+from src.presentation.api_bot.bot import bot_main
 
 if __name__ == "__main__":
-    asyncio.run(bot_main())
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(bot_main())
+    except KeyboardInterrupt:
+        print('Exit')
--- src/presentation/api_bot/handlers/hanlers.py
+++ src/presentation/api_bot/handlers/hanlers.py
@@ -1,3 +1,9 @@
-from aiogram import Bot, Dispatcher, Router
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
 
 router = Router()
+
+@router.message(CommandStart)
+async def cmd_start(message: Message):
+    await message.reply(f"Hi user {message.from_user.id}")
